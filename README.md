f7d8060cd1c8d073113c1560ef69d159d8cdc90c
Laura Neale <lauran@brandwatch.com>
2021-09-24 15:22:40 +0100

    question DB setup instructions and contents

---
+++ README.md
@@ -0,0 +1,9 @@
+# Doctor Quarantino
+
+
+
+## Setting up
+
+1. Create a database using the `db-setup.sql` file
+2. Load the database with questions. You can use the questions.txt file from this repo, or your own.
+3. Optionally, load up some recurring questions. These will be asked at the same time each week, Ye
--- db-setup.sql
+++ db-setup.sql
@@ -5,8 +5,3 @@ CREATE TABLE discord_chatbot_questions (
     recurring_slot int DEFAULT NULL,
     UNIQUE (question)
 );
-
-INSERT INTO discord_chatbot_questions (question, recurring_slot) VALUES
-('How was your weekend?', 1),
-('What tv show/book/music/etc have you been enjoying recent?', 6),
-('Show me your best gif!',9);
